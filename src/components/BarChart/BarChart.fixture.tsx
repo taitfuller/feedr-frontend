51d49eb733cff94f5ae02b9df4d4947c4279d95c
Di Kun Ong <dikuno10two@gmail.com>
2021-07-08 15:25:37 +1200

    Add labels to BarChart

--- src/components/BarChart/BarChart.fixture.tsx
+++ src/components/BarChart/BarChart.fixture.tsx
@@ -1,4 +1,13 @@
 import React from "react";
+import { useValue } from "react-cosmos/fixture";
 import BarChart from "./index";
 
-export default <BarChart featureCount={1000} bugCount={700} otherCount={300} />;
+const BarChartFixture = (): JSX.Element => {
+  const [feature] = useValue("feature", { defaultValue: 1000 });
+  const [bug] = useValue("bug", { defaultValue: 700 });
+  const [other] = useValue("other", { defaultValue: 300 });
+
+  return <BarChart featureCount={feature} bugCount={bug} otherCount={other} />;
+};
+
+export default BarChartFixture;
--- src/components/BarChart/index.tsx
+++ src/components/BarChart/index.tsx
@@ -14,20 +14,40 @@ const BarChart: React.FC<BarChartProps> = ({
 }: BarChartProps) => {
   const totalReviews = featureCount + bugCount + otherCount;
 
-  const featureFrac = (featureCount / totalReviews) * 100;
-  const bugFrac = ((featureCount + bugCount) / totalReviews) * 100;
+  const featureFrac = Math.round((featureCount / totalReviews) * 100);
+  const bugFrac = Math.round((bugCount / totalReviews) * 100);
+  const otherFrac = Math.round((otherCount / totalReviews) * 100);
+
+  const featureWidth = (featureCount / totalReviews) * 100;
+  const bugWidth = ((featureCount + bugCount) / totalReviews) * 100;
+
+  const featureLabelPos = 0;
+  const bugLabelPos = (featureCount / totalReviews) * 100;
+  const otherLabelPos = ((featureCount + bugCount) / totalReviews) * 100;
 
   return (
     <div>
       <div className={styles.barContainer}>
         <div
           className={`${styles.barFrac} ${styles.barBug}`}
-          style={{ width: `${bugFrac}%` }}
+          style={{ width: `${bugWidth}%` }}
         />
         <div
           className={`${styles.barFrac} ${styles.barFeature}`}
-          style={{ width: `${featureFrac}%` }}
+          style={{ width: `${featureWidth}%` }}
         />
+      </div>
+      <div>
+        <div className={styles.barLabel} style={{ left: featureLabelPos }}>
+          <span className={styles.percent}>{featureFrac}%</span> feature
+          requests
+        </div>
+        <div className={styles.barLabel} style={{ left: `${bugLabelPos}%` }}>
+          <span className={styles.percent}>{bugFrac}%</span> bug reports
+        </div>
+        <div className={styles.barLabel} style={{ left: `${otherLabelPos}%` }}>
+          <span className={styles.percent}>{otherFrac}%</span> other
+        </div>
       </div>
     </div>
   );
