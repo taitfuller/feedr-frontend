3e005915a1887c6c2d924c538a1e39db475aa5a6
Di Kun Ong <dikuno10two@gmail.com>
2021-07-13 00:46:01 +1200

    Add DropdownContainer UI component (styling incomplete)

---
+++ src/components/DropdownContainer/DropdownContainer.fixture.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import DropdownContainer from "./index";
+
+export default (
+  <>
+    <DropdownContainer title="Logged in as taitfuller">
+      <div style={{ padding: `12px 24px` }}>Test</div>
+    </DropdownContainer>
+    <div>Background div 1</div>
+    <div>Background div 2</div>
+    <div>Background div 3</div>
+    <div>Background div 4</div>
+    <div>Background div 5</div>
+  </>
+);
---
+++ src/components/DropdownContainer/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import styles from "./style.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+
+interface DropdownContainerProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const DropdownContainer: React.FC<DropdownContainerProps> = ({
+  title,
+  children,
+}: DropdownContainerProps): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className={styles.dropdown}>
+      <button className={styles.button} onClick={() => setIsOpen(!isOpen)}>
+        <span className={styles.text}>{title}</span>
+        <FontAwesomeIcon
+          icon={faCaretDown}
+          rotation={isOpen ? 180 : undefined}
+        />
+      </button>
+      <div className={`${styles.children} ${isOpen && styles.childrenOpen}`}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default DropdownContainer;
