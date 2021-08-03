175f168018ccf71a449ad6ae1f4026e682ca1280
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2021-08-03 22:01:46 +1200

    Improve DropdownContainer styling and functionality

--- src/components/DropdownContainer/index.tsx
+++ src/components/DropdownContainer/index.tsx
@@ -1,29 +1,49 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./style.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 interface DropdownContainerProps {
-  title: string;
+  title: string | React.ReactChild;
   children: React.ReactNode;
+  width?: number;
+  overflow: "none" | "left" | "right";
+  hideButtonWhenOpen?: boolean;
+  isOpen: boolean;
+  setIsOpen: (arg0: boolean) => void;
 }
 
 const DropdownContainer: React.FC<DropdownContainerProps> = ({
   title,
   children,
+  width,
+  overflow,
+  hideButtonWhenOpen = false,
+  isOpen,
+  setIsOpen,
 }: DropdownContainerProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <div className={styles.dropdown}>
-      <button className={styles.button} onClick={() => setIsOpen(!isOpen)}>
-        <span className={styles.text}>{title}</span>
+    <div className={styles.dropdown} style={{ width: width && `${width}px` }}>
+      <button
+        className={`${styles.button} ${
+          !hideButtonWhenOpen && styles.buttonShowWhenOpen
+        }`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {title}
         <FontAwesomeIcon
+          className={styles.dropdownCaret}
           icon={faCaretDown}
           rotation={isOpen ? 180 : undefined}
         />
       </button>
-      <div className={`${styles.children} ${isOpen && styles.childrenOpen}`}>
+      <div
+        className={`${styles.children} ${isOpen && styles.childrenOpen} ${
+          overflow === "none" && styles.childrenNoOverflow
+        } ${overflow === "left" && styles.childrenLeftOverflow} ${
+          overflow === "right" && styles.childrenRightOverflow
+        }`}
+      >
         {children}
       </div>
     </div>
