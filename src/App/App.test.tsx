89221274460e1184aba262fc37cb12adc89b3df8
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2021-09-21 21:39:43 +1200

    Fix App tests

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,26 +1,42 @@
 import React from "react";
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import renderWithRouter from "../util/renderWithRouter";
 import App from "./index";
 
 jest.mock("../pages/DashboardPage");
 
+afterEach(() => {
+  localStorage.clear();
+});
+
 describe("App", () => {
-  it("Displays DashboardPage when route is `/dashboard`", () => {
+  it("Displays DashboardPage when route is `/dashboard` and authenticated", () => {
+    localStorage.setItem("token", "let.me.in");
+
     renderWithRouter(<App />, "/dashboard");
 
     expect(screen.getByTestId("dashboard-page")).toBeTruthy();
   });
 
-  it("Displays DashboardPage when route is `/dashboard/sub-route`", () => {
+  it("Displays DashboardPage when route is `/dashboard/sub-route` and authenticated", () => {
+    localStorage.setItem("token", "let.me.in");
+
     renderWithRouter(<App />, "/dashboard/sub-route");
 
     expect(screen.getByTestId("dashboard-page")).toBeTruthy();
   });
 
-  it("Redirects to `/dashboard` when route is `/`", () => {
+  it("Redirects to `/dashboard` when route is `/` and authenticated", () => {
+    localStorage.setItem("token", "let.me.in");
+
     const { history } = renderWithRouter(<App />, "/");
 
     expect(history.location.pathname).toBe("/dashboard");
+  });
+
+  it("Redirects to `/login` when not authenticated", () => {
+    const { history } = renderWithRouter(<App />, "/");
+
+    expect(history.location.pathname).toBe("/login");
   });
 });
