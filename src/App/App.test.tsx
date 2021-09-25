cd4500e9c6c7dad3105a7954f37921c9fd9012d9
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2021-09-25 14:57:00 +1200

    Fix frontend routing tests

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -4,39 +4,66 @@ import renderWithRouter from "../util/renderWithRouter";
 import App from "./index";
 
 jest.mock("../pages/DashboardPage");
+jest.mock("../pages/LoginPage");
+jest.mock("../pages/OnboardPage");
 
 afterEach(() => {
   localStorage.clear();
 });
 
 describe("App", () => {
-  it("Displays DashboardPage when route is `/dashboard` and authenticated", () => {
+  it("Displays DashboardPage when route is `/dashboard/some-feed-id` and authenticated", () => {
     localStorage.setItem("token", "let.me.in");
 
-    renderWithRouter(<App />, "/dashboard");
+    renderWithRouter(<App />, "/dashboard/some-feed-id");
 
     expect(screen.getByTestId("dashboard-page")).toBeTruthy();
   });
 
-  it("Displays DashboardPage when route is `/dashboard/sub-route` and authenticated", () => {
+  it("Redirects to OnboardPage when route is `/dashboard` and authenticated", () => {
     localStorage.setItem("token", "let.me.in");
 
-    renderWithRouter(<App />, "/dashboard/sub-route");
+    const { history } = renderWithRouter(<App />, "/dashboard");
 
-    expect(screen.getByTestId("dashboard-page")).toBeTruthy();
+    expect(history.location.pathname).toBe("/onboard");
+    expect(screen.getByTestId("onboard-page")).toBeTruthy();
   });
 
-  it("Redirects to `/dashboard` when route is `/` and authenticated", () => {
+  it("Displays OnboardPage when route is `/onboard` and authenticated", () => {
+    localStorage.setItem("token", "let.me.in");
+
+    renderWithRouter(<App />, "/onboard");
+
+    expect(screen.getByTestId("onboard-page")).toBeTruthy();
+  });
+
+  it("Redirects to `/onboard` when route is `/` and authenticated", () => {
     localStorage.setItem("token", "let.me.in");
 
     const { history } = renderWithRouter(<App />, "/");
 
-    expect(history.location.pathname).toBe("/dashboard");
+    expect(history.location.pathname).toBe("/onboard");
+    expect(screen.getByTestId("onboard-page")).toBeTruthy();
   });
 
-  it("Redirects to `/login` when not authenticated", () => {
+  it("Redirects to `/login` when route is `/` and not authenticated", () => {
     const { history } = renderWithRouter(<App />, "/");
 
     expect(history.location.pathname).toBe("/login");
+    expect(screen.getByTestId("login-page")).toBeTruthy();
+  });
+
+  it("Redirects to `/login` when route is `/onboard` and not authenticated", () => {
+    const { history } = renderWithRouter(<App />, "/onboard");
+
+    expect(history.location.pathname).toBe("/login");
+    expect(screen.getByTestId("login-page")).toBeTruthy();
+  });
+
+  it("Redirects to `/login` when route is `/dashboard/:id` and not authenticated", () => {
+    const { history } = renderWithRouter(<App />, "/dashboard/:id");
+
+    expect(history.location.pathname).toBe("/login");
+    expect(screen.getByTestId("login-page")).toBeTruthy();
   });
 });
---
+++ src/pages/LoginPage/__mocks__/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LoginPage: React.FC = () => <div data-testid="login-page" />;
+
+export default LoginPage;
